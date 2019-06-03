39b42da52e37d995bfc9c5394b18a484b2bc9d98
thiele <chris.thiele@gmail.com>
2019-06-03 01:11:53 -0700

    Add files via upload

--- sensors/ultrasonic.py
+++ sensors/ultrasonic.py
@@ -1,37 +1,52 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 
 # Ultrasonic Ranging Module / Distance Sensor HC - SR04, takes TRIG (raspberry pi to sensor), and outputs ECHO (sensor to raspberry pi @ 5V, be sure to have appropriate resistors set up)
 
-def getdistance(TRIG, ECHO):
-    GPIO.setmode(GPIO.BCM)
+TRIG = 21
+ECHO = 18
 
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+# def init_ultrasonic():
 
-    GPIO.output(TRIG,True)
-    time.sleep(0.0001)
-    GPIO.output(TRIG, False)
+gpio.setmode(gpio.BCM)
 
-    while GPIO.input(ECHO) == False:
+gpio.setup(TRIG, gpio.OUT)
+gpio.setup(ECHO, gpio.IN)
+
+def getdist_ultrasonic():
+    timeout = time.time() + .1
+    gpio.output(TRIG,True)
+    time.sleep(0.0001) # no need to extend signal.
+    gpio.output(TRIG, False)
+
+    start = time.time()
+    end = time.time()
+
+    while gpio.input(ECHO) == False:
         start = time.time()
+        if time.time() > timeout:
+            break
 
-    while GPIO.input(ECHO) == True:
+    while gpio.input(ECHO) == True:
         end = time.time()
+        if time.time() > timeout:
+            break
 
     difftime = end - start
 
     # distance = velocity * time. Speed of sound at 20C (68F) and 0% humidity is 343 m/s. Distance is round trip (sonar) so is divided by 2.
 
-    distance = (17150 * difftime)
-
-    GPIO.cleanup()
-
+    if difftime >.09:
+        distance = 0
+    else:
+        distance = (17150 * difftime)
     return distance
 
+def reset_ultrasonic():
+    gpio.cleanup()
 
 if __name__ == "__main__":
-    TRIG = 4
-    ECHO = 18
-    dx = getdistance(TRIG,ECHO)
+    dx = getdist_ultrasonic()
     print('Distance: {} centimeters'.format(dx))
+    print(dx)
+    reset_ultrasonic()
