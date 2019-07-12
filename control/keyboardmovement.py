f363150ff193e73afbda17653a4c083522a8dc20
thiele <chris.thiele@gmail.com>
2019-07-11 22:13:17 -0700

    Add files via upload
    separated out core response functions to processkey.py

--- control/keyboardmovement.py
+++ control/keyboardmovement.py
@@ -1,11 +1,7 @@
-import RPi.GPIO as gpio
-from drivepwm import *
 import curses
 import os
-from ultrasonic import *
-from servo import *
 import power
-import orientation
+import processkey
 
 # intializes curses globally.
 
@@ -15,7 +11,6 @@ stdscr = curses.initscr()
 os.system("sudo service motion start")
 
 def keyboard():
-
     # power saving - turn off idle services
     result = power.powercheck()
     if result is True:
@@ -29,46 +24,13 @@ def keyboard():
     curses.cbreak()
     stdscr.keypad(1)
 
-    degree = 0
-
-    # get default magnet calibration data
-    calibratedtime, C, ymax, normal = orientation.checkmagnetdata()
-
     while 1:
         c = stdscr.getch()
-        if c == ord('w'):
-            forward(sec)
-        elif c == ord('s'):
-            reverse(sec)
-        elif c == ord('a'):
-            pivotleft(sec)
-        elif c == ord('d'):
-            pivotright(sec)
-        elif c == ord('q'):
-            partialleft(sec, 50)
-        elif c == ord('e'):
-            partialright(sec, 50)
-        elif c == ord('g'):
-            coordsample, angle = orientation.getmedianmagnet(C, ymax, normal)
-            print("Angle =", angle)
-        elif c == ord('x'):
-            stop(sec)
-        elif c == ord('r'):
+        result = chr(c)
+        processresult = processkey.processkey(result)
+        if processresult is False:
+            endkeyboard()
             break
-        elif c == ord('p'):
-            print("Hello World")
-        elif c == ord('f'):
-            dx = getdist_ultrasonic()
-            print(dx)
-        elif c == curses.KEY_LEFT:
-            degree = degree - 15
-            degree = movebydegree(sec, degree)
-        elif c == 350: #unicode integer for keypad center
-            degree = movebydegree(sec, 0)
-        elif c == curses.KEY_RIGHT:
-            degree = degree + 15
-            degree = movebydegree(sec, degree)
-    endkeyboard()
 
 def endkeyboard():
     #reset settings
@@ -77,13 +39,6 @@ def endkeyboard():
     curses.echo()
     #cleanup curses
     curses.endwin()
-    #reset motor
-    endmotor()
-    #reset servo
-    endservo()
-    #cleanup GPIO
-    gpio.cleanup()
-
     #exit motion (webcam)
     os.system("sudo service motion stop")
 
